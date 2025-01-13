c77db3f3e0849fd28193a9641f651a4a3b6dc8f5
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2025-01-13 16:14:37 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/user');
 
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
